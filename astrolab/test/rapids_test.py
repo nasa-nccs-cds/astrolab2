d7f8a82ff5e6781365a6a3491fe90e54fb349b20
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2020-11-11 16:06:48 -0500

    Add rapids test

--- astrolab/test/rapids_test.py
+++ astrolab/test/rapids_test.py
@@ -22,7 +22,7 @@ color_key = {str(d): c for d, c in enumerate(pal)}
 
 # Using cudf Dataframe here is not likely to help with performance
 # However, it's a good opportunity to get familiar with the API
-source_df = cudf.read_csv('/att/nobackup/tpmaxwel/data/fashion-mnist-csv/fashion_train.csv')
+source_df: cudf.DataFrame = cudf.read_csv('/att/nobackup/tpmaxwel/data/fashion-mnist-csv/fashion_train.csv')
 data = source_df.loc[ :, source_df.columns[:-1] ]
 target = source_df[ source_df.columns[-1] ]
 
@@ -48,33 +48,35 @@ target = source_df[ source_df.columns[-1] ]
 
 reducer = cuml.UMAP(
     n_neighbors=15,
-    n_components=2,
+    n_components=3,
     n_epochs=500,
-    min_dist=0.1
+    min_dist=0.1,
+    output_type="numpy"
 )
 embedding = reducer.fit_transform(data)
+print(f"Completed embedding, shape = {embedding.shape}")
 
-df = embedding.to_pandas()
-df.columns = ["x", "y"]
-df['class'] = pd.Series([str(x) for x in target.to_array()], dtype="category")
-
-cvs = ds.Canvas(plot_width=400, plot_height=400)
-agg = cvs.points(df, 'x', 'y', ds.count_cat('class'))
-img = tf.shade(agg, color_key=color_key, how='eq_hist')
-
-utils.export_image(img, filename='fashion-mnist', background='black')
-
-image = plt.imread('fashion-mnist.png')
-fig, ax = plt.subplots(figsize=(12, 12))
-plt.imshow(image)
-plt.setp(ax, xticks=[], yticks=[])
-plt.title("Fashion MNIST data embedded\n"
-          "into two dimensions by UMAP\n"
-          "visualised with Datashader",
-          fontsize=12)
-
-plt.show()
-
+# df = embedding.to_pandas()
+# df.columns = ["x", "y"]
+# df['class'] = pd.Series([str(x) for x in target.to_array()], dtype="category")
+#
+# cvs = ds.Canvas(plot_width=400, plot_height=400)
+# agg = cvs.points(df, 'x', 'y', ds.count_cat('class'))
+# img = tf.shade(agg, color_key=color_key, how='eq_hist')
+#
+# utils.export_image(img, filename='fashion-mnist', background='black')
+#
+# image = plt.imread('fashion-mnist.png')
+# fig, ax = plt.subplots(figsize=(12, 12))
+# plt.imshow(image)
+# plt.setp(ax, xticks=[], yticks=[])
+# plt.title("Fashion MNIST data embedded\n"
+#           "into two dimensions by UMAP\n"
+#           "visualised with Datashader",
+#           fontsize=12)
+#
+# plt.show()
+#
 
 
 
