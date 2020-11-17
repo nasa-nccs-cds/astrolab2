0a702e0f4e1f05fe7db91cc661c2b863f8ab0950
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2020-11-17 12:38:52 -0500

    Add gpu umap

--- astrolab/test/umap.py
+++ astrolab/test/umap.py
@@ -6,14 +6,14 @@ import cudf, cuml, cupy, cupyx
 
 app = Astrolab.instance()
 app.configure("spectraclass")
-n_neighbors = 5
+n_neighbors = 15
 project_dataset: xa.Dataset = DataManager.instance().loadCurrentProject("spectraclass")
 umap_data: xa.DataArray = project_dataset["reduction"].compute()
 
 t0 = time.time()
 print( f"Computing embedding, input shape = {umap_data.shape}" )
 input_data = cudf.DataFrame({ icol : umap_data[:,icol] for icol in range(umap_data.shape[1]) } )
-reducer = cuml.UMAP( n_neighbors=15, n_components=3, n_epochs=500, min_dist=0.1, output_type="numpy" )
+reducer = cuml.UMAP( init="spectral", n_neighbors=n_neighbors, n_components=3, n_epochs=500, min_dist=0.1, output_type="numpy" )
 embedding = reducer.fit_transform( input_data )
 print( f"Completed embedding in time {time.time()-t0} sec, embedding shape = {embedding.shape}" )
 
