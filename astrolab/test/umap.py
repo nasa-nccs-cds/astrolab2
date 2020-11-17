73291d1a34ca9571a803e1d994faf074563d5834
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2020-11-17 12:13:53 -0500

    Add gpu umap

--- astrolab/test/umap.py
+++ astrolab/test/umap.py
@@ -15,7 +15,7 @@ project_dataset: xa.Dataset = DataManager.instance().loadCurrentProject("spectra
 
 umap_data: xa.DataArray = project_dataset["reduction"]
 print( f"Computing embedding, input shape = {umap_data.shape}" )
-input_data =  cudf.from_pandas( umap_data.to_dataframe() )
+input_data = cudf.DataFrame({ icol : umap_data[:,icol] for icol in range(umap_data.shape[1]) } )
 reducer = cuml.UMAP( n_neighbors=15, n_components=3, n_epochs=500, min_dist=0.1, output_type="numpy" )
 embedding = reducer.fit_transform( input_data )
 print( f"Completed embedding, shape = {embedding.shape}" )
